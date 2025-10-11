b86788203ee39580695add618f23583366b03e10
Wesly <agustchannel@gmail.com>
2025-10-11 09:08:30 +0700

    feat: wall brightness based on distance

--- main.go
+++ main.go
@@ -73,154 +73,165 @@ var Plane = Vec2{0, 0.66}
 
 var floorCeilTexture rl.Texture2D
 
+func RenderFloor() {
+	var y float64
+	for y = SCREEN_HEIGHT / 2; y < SCREEN_HEIGHT; y++ {
+		var floorBuffer []rl.Color = make([]rl.Color, SCREEN_WIDTH)
+		// var ceilBuffer []rl.Color = make([]rl.Color, SCREEN_WIDTH)
+		rayBegin := Dir.Sub(Plane)
+		rayEnd := Dir.Add(Plane)
+
+		p := y - SCREEN_HEIGHT/2
+		posZ := float64(0.5 * SCREEN_HEIGHT)
+		rowDistance := posZ / p
+		stepSizeX := rowDistance * (rayEnd.X - rayBegin.X) / SCREEN_WIDTH
+		stepSizeY := rowDistance * (rayEnd.Y - rayBegin.Y) / SCREEN_WIDTH
+
+		wallX := Pos.X + rowDistance*rayBegin.X
+		wallY := Pos.Y + rowDistance*rayBegin.Y
+
+		for x := range SCREEN_WIDTH {
+			cellX := math.Floor(wallX)
+			cellY := math.Floor(wallY)
+
+			texX := int32(FLOOR_TEX_WIDTH*(wallX-cellX)) & (FLOOR_TEX_WIDTH - 1)
+			texY := int32(FLOOR_TEX_HEIGHT*(wallY-cellY)) & (FLOOR_TEX_HEIGHT - 1)
+
+			wallX += stepSizeX
+			wallY += stepSizeY
+
+			floorColor := floorTexture[int(texY)*int(FLOOR_TEX_WIDTH)+int(texX)]
+			floorBuffer[x] = floorColor
+		}
+		rl.UpdateTextureRec(floorCeilTexture, rl.NewRectangle(0, float32(y), SCREEN_WIDTH, 1), floorBuffer)
+	}
+	rl.DrawTexture(floorCeilTexture, 0, 0, rl.White)
+
+}
+
+func RenderWall() {
+	var x float64
+	for x = 0; x < SCREEN_WIDTH; x++ {
+		cameraX := ((2 * x) / float64(SCREEN_WIDTH)) - 1 // cameraX : -1...0...1
+		rayDir := Dir.Add(Plane.Mul(cameraX))
+
+		mapX := int32(Pos.X)
+		mapY := int32(Pos.Y)
+
+		var deltaDist Vec2
+
+		if rayDir.X == 0 {
+			deltaDist.X = math.Inf(1)
+		} else {
+			deltaDist.X = math.Abs(1 / rayDir.X)
+		}
+		if rayDir.Y == 0 {
+			deltaDist.Y = math.Inf(1)
+		} else {
+			deltaDist.Y = math.Abs(1 / rayDir.Y)
+		}
+
+		var sideDist Vec2
+		var stepX int32
+		var stepY int32
+
+		if rayDir.X > 0 {
+			stepX = 1
+			sideDist.X = (float64(mapX+1) - Pos.X) * deltaDist.X
+		} else {
+			stepX = -1
+			sideDist.X = (Pos.X - float64(mapX)) * deltaDist.X
+		}
+		if rayDir.Y > 0 {
+			stepY = 1
+			sideDist.Y = (float64(mapY+1) - Pos.Y) * deltaDist.Y
+		} else {
+			stepY = -1
+			sideDist.Y = (Pos.Y - float64(mapY)) * deltaDist.Y
+		}
+
+		var side int8 = -1
+		var hittedWall int
+
+		for true {
+			if sideDist.X < sideDist.Y {
+				sideDist.X += deltaDist.X
+				mapX += stepX
+				side = 0
+			} else {
+				drawRay(rayDir.Mul(sideDist.Y))
+				sideDist.Y += deltaDist.Y
+				mapY += stepY
+				side = 1
+			}
+			if MAP[mapY][mapX] > 0 {
+				hittedWall = MAP[mapY][mapX]
+				break
+			}
+		}
+
+		var perpWallDist float64
+		if side == 0 {
+			perpWallDist = sideDist.X - deltaDist.X
+		} else {
+			perpWallDist = sideDist.Y - deltaDist.Y
+		}
+
+		var factor float64 = 1.25
+		if perpWallDist > 0 {
+			res := 1.75 / perpWallDist
+			if res < factor {
+				factor = res
+			}
+		}
+
+		wallHeight := SCREEN_HEIGHT / perpWallDist
+		wallStart := (-wallHeight / 2) + SCREEN_HEIGHT/2
+		wallEnd := (wallHeight / 2) + SCREEN_HEIGHT/2
+
+		if wallStart < 0 {
+			wallStart = 0
+		}
+		if wallEnd >= SCREEN_HEIGHT {
+			wallEnd = SCREEN_HEIGHT - 1
+		}
+
+		var wallX float64
+		if side == 0 {
+			wallX = Pos.Y + perpWallDist*rayDir.Y
+		} else {
+			wallX = Pos.X + perpWallDist*rayDir.X
+		}
+		wallX -= math.Floor(wallX)
+		texX := int(wallX * float64(TEX_WIDTH))
+
+		step := 1.0 * TEX_HEIGHT / wallHeight
+		texPos := (wallStart - float64(SCREEN_HEIGHT)/2 + wallHeight/2) * step
+		selectedTex := textures[hittedWall]
+		texY := int(texPos) & (TEX_HEIGHT - 1)
+
+		tintVal := 750 / perpWallDist
+		tintVal = math.Max(tintVal, 50)
+		tintVal = math.Min(tintVal, 255)
+		tint := rl.NewColor(uint8(tintVal), uint8(tintVal), uint8(tintVal), 255)
+
+		rl.DrawTexturePro(
+			selectedTex, 
+			rl.NewRectangle(float32(texX), float32(texY), 1, TEX_HEIGHT),
+			rl.NewRectangle(float32(x), float32(wallStart), 1, float32(wallHeight)),
+			rl.Vector2{X: 0, Y: 0}, 
+			0.0, 
+			tint,
+		)
+
+		drawRay(rayDir)
+	}
+
+}
 
 func RenderScene() {
-	// Floor cast
-	{
-		var y float64
-		for y = SCREEN_HEIGHT / 2; y < SCREEN_HEIGHT; y++ {
-			var floorBuffer []rl.Color = make([]rl.Color, SCREEN_WIDTH)
-			// var ceilBuffer []rl.Color = make([]rl.Color, SCREEN_WIDTH)
-			rayBegin := Dir.Sub(Plane)
-			rayEnd := Dir.Add(Plane)
-
-			p := y - SCREEN_HEIGHT/2
-			posZ := float64(0.5 * SCREEN_HEIGHT)
-			rowDistance := posZ / p
-			stepSizeX := rowDistance * (rayEnd.X - rayBegin.X) / SCREEN_WIDTH
-			stepSizeY := rowDistance * (rayEnd.Y - rayBegin.Y) / SCREEN_WIDTH
-
-			wallX := Pos.X + rowDistance*rayBegin.X
-			wallY := Pos.Y + rowDistance*rayBegin.Y
-
-			for x := range SCREEN_WIDTH {
-				cellX := math.Floor(wallX)
-				cellY := math.Floor(wallY)
-
-				texX := int32(FLOOR_TEX_WIDTH*(wallX-cellX)) & (FLOOR_TEX_WIDTH - 1)
-				texY := int32(FLOOR_TEX_HEIGHT*(wallY-cellY)) & (FLOOR_TEX_HEIGHT - 1)
-
-				wallX += stepSizeX
-				wallY += stepSizeY
-
-				floorColor := floorTexture[int(texY)*int(FLOOR_TEX_WIDTH)+int(texX)]
-				floorBuffer[x] = floorColor
-			}
-			rl.UpdateTextureRec(floorCeilTexture, rl.NewRectangle(0, float32(y), SCREEN_WIDTH, 1), floorBuffer)
-		}
-		rl.DrawTexture(floorCeilTexture, 0, 0, rl.White)
-	}
-
-	// Wall cast
-	{
-		var x float64
-		for x = 0; x < SCREEN_WIDTH; x++ {
-			cameraX := ((2 * x) / float64(SCREEN_WIDTH)) - 1 // cameraX : -1...0...1
-			rayDir := Dir.Add(Plane.Mul(cameraX))
-
-			mapX := int32(Pos.X)
-			mapY := int32(Pos.Y)
-
-			var deltaDist Vec2
-
-			if rayDir.X == 0 {
-				deltaDist.X = math.Inf(1)
-			} else {
-				deltaDist.X = math.Abs(1 / rayDir.X)
-			}
-			if rayDir.Y == 0 {
-				deltaDist.Y = math.Inf(1)
-			} else {
-				deltaDist.Y = math.Abs(1 / rayDir.Y)
-			}
-
-			var sideDist Vec2
-			var stepX int32
-			var stepY int32
-
-			if rayDir.X > 0 {
-				stepX = 1
-				sideDist.X = (float64(mapX+1) - Pos.X) * deltaDist.X
-			} else {
-				stepX = -1
-				sideDist.X = (Pos.X - float64(mapX)) * deltaDist.X
-			}
-			if rayDir.Y > 0 {
-				stepY = 1
-				sideDist.Y = (float64(mapY+1) - Pos.Y) * deltaDist.Y
-			} else {
-				stepY = -1
-				sideDist.Y = (Pos.Y - float64(mapY)) * deltaDist.Y
-			}
-
-			var side int8 = -1
-			var hittedWall int
-
-			for true {
-				if sideDist.X < sideDist.Y {
-					sideDist.X += deltaDist.X
-					mapX += stepX
-					side = 0
-				} else {
-					drawRay(rayDir.Mul(sideDist.Y))
-					sideDist.Y += deltaDist.Y
-					mapY += stepY
-					side = 1
-				}
-				if MAP[mapY][mapX] > 0 {
-					hittedWall = MAP[mapY][mapX]
-					break
-				}
-			}
-
-			var perpWallDist float64
-			if side == 0 {
-				perpWallDist = sideDist.X - deltaDist.X
-			} else {
-				perpWallDist = sideDist.Y - deltaDist.Y
-			}
-
-			var factor float64 = 1.25
-			if perpWallDist > 0 {
-				res := 1.75 / perpWallDist
-				if res < factor {
-					factor = res
-				}
-			}
-
-			wallHeight := SCREEN_HEIGHT / perpWallDist
-			wallStart := (-wallHeight / 2) + SCREEN_HEIGHT/2
-			wallEnd := (wallHeight / 2) + SCREEN_HEIGHT/2
-
-			if wallStart < 0 {
-				wallStart = 0
-			}
-			if wallEnd >= SCREEN_HEIGHT {
-				wallEnd = SCREEN_HEIGHT - 1
-			}
-
-			var wallX float64
-			if side == 0 {
-				wallX = Pos.Y + perpWallDist*rayDir.Y
-			} else {
-				wallX = Pos.X + perpWallDist*rayDir.X
-			}
-			wallX -= math.Floor(wallX)
-			texX := int(wallX * float64(TEX_WIDTH))
-
-			step := 1.0 * TEX_HEIGHT / wallHeight
-			texPos := (wallStart - float64(SCREEN_HEIGHT)/2 + wallHeight/2) * step
-			selectedTex := textures[hittedWall]
-
-			texY := int(texPos) & (TEX_HEIGHT - 1)
-
-			rl.DrawTexturePro(selectedTex, rl.NewRectangle(float32(texX), float32(texY), 1, TEX_HEIGHT),
-				rl.NewRectangle(float32(x), float32(wallStart), 1, float32(wallHeight)), rl.Vector2{X: 0, Y: 0}, 0.0, rl.White)
-
-			drawRay(rayDir)
-		}
-	}
-
+	RenderFloor()
+	RenderWall()
 }
 
 func mapCoordToPixel(vec Vec2) Vec2 {
@@ -395,7 +406,7 @@ func InitTexture() {
 	floorTexture = rl.LoadImageColors(rl.LoadImageFromTexture(rl.LoadTexture("./assets/aot/floor.png")))
 	// ceilTexture = rl.LoadImageColors(rl.LoadImageFromTexture(wood))
 	bgTexture = rl.LoadTexture("./assets/aot/bg.png")
-	floorCeilTexture = rl.LoadTextureFromImage(rl.GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, rl.SkyBlue))
+	floorCeilTexture = rl.LoadTextureFromImage(rl.GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, rl.NewColor(0, 0, 104, 255)))
 }
 
 func main() {
